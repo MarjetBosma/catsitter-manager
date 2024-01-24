e474e995497b8ba21504cbc06285df0a50bf18b1
MarjetBosma <marjet_bosma@hotmail.com>
2024-01-24 15:51:57 +0100

    structuur project aangemaakt incl dependencies

---
+++ src/main/java/nl/novi/catsittermanager/CatsittermanagerApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.catsittermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsittermanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatsittermanagerApplication.class, args);
+    }
+
+}
