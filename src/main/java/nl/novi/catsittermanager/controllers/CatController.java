59cdfea36b5c6c6b55baf64fa83720659c843e6d
MarjetBosma <marjet_bosma@hotmail.com>
2024-01-31 10:48:00 +0100

    CRUD operaties toegevoegd aan controllers

--- src/main/java/nl/novi/catsittermanager/controllers/CatController.java
+++ src/main/java/nl/novi/catsittermanager/controllers/CatController.java
@@ -1,10 +1,80 @@
 package nl.novi.catsittermanager.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import nl.novi.catsittermanager.dtos.IdInputDto;
+import nl.novi.catsittermanager.dtos.cat.CatDto;
+import nl.novi.catsittermanager.dtos.cat.CatInputDto;
+import nl.novi.catsittermanager.exceptions.RecordNotFoundException;
+import nl.novi.catsittermanager.exceptions.ValidationException;
+import nl.novi.catsittermanager.services.CatService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+import static nl.novi.catsittermanager.controllers.ControllerHelper.checkForBindingResult;
 
 @RestController
 @RequestMapping("/cats")
 
 public class CatController {
+
+    private final CatService catService;
+
+    public CatController(CatService catService) {
+        this.catService = catService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CatDto>> getAllCats() {
+        return ResponseEntity.ok(catService.getAllCats());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CatDto> getCat(@PathVariable Long id) {
+        if (id > 0) {
+            CatDto catDto = CatService.getCatId();
+            return ResponseEntity.ok(catDto);
+        } else {
+            throw new RecordNotFoundException("No cat found with this id");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<CatDto> addCat(@RequestBody CatInputDto catInputDto, BindingResult br) {
+        if (br.hasFieldErrors()) {
+            throw new ValidationException(checkForBindingResult(br));
+        } else {
+            CatDto savedCat;
+            savedCat = catService.createCat(catInputDto);
+            URI uri = URI.create(
+                    ServletUriComponentsBuilder
+                            .fromCurrentRequest()
+                            .path("/" + savedCat.id).toUriString());
+            return ResponseEntity.created(uri).body(savedCat);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CatDto> updateCat(@PathVariable long id, @RequestBody CatInputDto cat) {
+        CatDto changeCIModuleId = catService.updateCat(id, cat);
+
+        return ResponseEntity.ok().body(changeCatId);
+    }
+
+    @PutMapping("/{id}/customer")
+    public ResponseEntity<Object> assignCustomerToCat(@PathVariable("id") Long id,@Valid @RequestBody IdInputDto input) {
+        catService.assignCustomerToCat(id, input.id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteCat(@PathVariable("id") Long id) {
+        catService.deleteCat(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/nl/novi/catsittermanager/controllers/TaskController.java
+++ src/main/java/nl/novi/catsittermanager/controllers/TaskController.java
@@ -1,10 +1,80 @@
 package nl.novi.catsittermanager.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import nl.novi.catsittermanager.dtos.IdInputDto;
+import nl.novi.catsittermanager.dtos.task.TaskDto;
+import nl.novi.catsittermanager.dtos.task.TaskInputDto;
+import nl.novi.catsittermanager.exceptions.RecordNotFoundException;
+import nl.novi.catsittermanager.exceptions.ValidationException;
+import nl.novi.catsittermanager.services.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+import static nl.novi.catsittermanager.controllers.ControllerHelper.checkForBindingResult;
 
 @RestController
 @RequestMapping("/tasks")
 
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskDto>> getAllTasks() {
+        return ResponseEntity.ok(taskService.getAllTasks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskDto> getTask(@PathVariable Long id) {
+        if (id > 0) {
+            TaskDto taskDto = TaskService.getTaskId();
+            return ResponseEntity.ok(taskDto);
+        } else {
+            throw new RecordNotFoundException("No task found with this id");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskDto> addTask(@RequestBody TaskInputDto taskInputDto, BindingResult br) {
+        if (br.hasFieldErrors()) {
+            throw new ValidationException(checkForBindingResult(br));
+        } else {
+            TaskDto savedTask;
+            savedTask = taskService.createTask(taskInputDto);
+            URI uri = URI.create(
+                    ServletUriComponentsBuilder
+                            .fromCurrentRequest()
+                            .path("/" + savedTask.id).toUriString());
+            return ResponseEntity.created(uri).body(savedTask);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskDto> updateTask(@PathVariable long id, @RequestBody TaskInputDto task) {
+        TaskDto changeTaskId = taskService.updateTask(id, task);
+
+        return ResponseEntity.ok().body(changeTaskId);
+    }
+
+    @PutMapping("/{id}/order")
+    public ResponseEntity<Object> assignOrderToTask(@PathVariable("id") Long id,@Valid @RequestBody IdInputDto input) {
+        taskService.assignOrderToTask(id, input.id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteTask(@PathVariable("id") Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
