95c76a35a3ba90dbc5664f790e54827d089a58f7
Bosma <marjet.bosma@sogeti.com>
2024-04-02 13:31:39 +0200

    CatMapper tests geschreven, alles gaat goed behalve field owner

--- src/test/java/nl/novi/catsittermanager/mappers/CatMapperTest.java
+++ src/test/java/nl/novi/catsittermanager/mappers/CatMapperTest.java
@@ -1,5 +1,10 @@
 package nl.novi.catsittermanager.mappers;
 
+import nl.novi.catsittermanager.dtos.cat.CatRequest;
+import nl.novi.catsittermanager.dtos.cat.CatRequestFactory;
+import nl.novi.catsittermanager.dtos.cat.CatResponse;
+import nl.novi.catsittermanager.models.Cat;
+import nl.novi.catsittermanager.models.CatFactory;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,12 +12,53 @@ import static org.junit.jupiter.api.Assertions.*;
 class CatMapperTest {
 
     @Test
-    void catToCatResponse() {
-        //Todo create test
+    void testCatToCatResponse() {
+        // Given
+        Cat cat = CatFactory.randomCat().build();
+
+        // When
+        CatResponse catResponse = CatMapper.CatToCatResponse(cat);
+
+        // Then
+        assertEquals(cat.getId(), catResponse.id());
+        assertEquals(cat.getName(), catResponse.name());
+        assertEquals(cat.getDateOfBirth(), catResponse.dateOfBirth());
+        assertEquals(cat.getGender(), catResponse.gender());
+        assertEquals(cat.getBreed(), catResponse.breed());
+        assertEquals(cat.getGeneralInfo(), catResponse.generalInfo());
+        assertEquals(cat.getSpayedOrNeutered(), catResponse.spayedOrNeutered());
+        assertEquals(cat.getVaccinated(), catResponse.vaccinated());
+        assertEquals(cat.getVeterinarianName(), catResponse.veterinarianName());
+        assertEquals(cat.getPhoneVet(), catResponse.phoneVet());
+        assertEquals(cat.getMedicationName(), catResponse.medicationName());
+        assertEquals(cat.getMedicationDose(), catResponse.medicationDose());
+        assertEquals(cat.getOwner(), catResponse.ownerUsername()); // hier gaat iets mis
     }
 
     @Test
-    void CatRequestToCat() {
-        //Todo create test
+    void testCatRequestToCat() {
+        // Given
+        CatRequest catRequest = CatRequestFactory.randomCatRequest().build();
+
+        // When
+        Cat cat = CatMapper.CatRequestToCat(catRequest);
+
+        // Then
+        assertEquals(catRequest.name(), cat.getName());
+        assertEquals(catRequest.dateOfBirth(), cat.getDateOfBirth());
+        assertEquals(catRequest.gender(), cat.getGender());
+        assertEquals(catRequest.dateOfBirth(), cat.getDateOfBirth());
+        assertEquals(catRequest.gender(), cat.getGender());
+        assertEquals(catRequest.breed(), cat.getBreed());
+        assertEquals(catRequest.generalInfo(), cat.getGeneralInfo());
+        assertEquals(catRequest.spayedOrNeutered(), cat.getSpayedOrNeutered());
+        assertEquals(catRequest.vaccinated(), cat.getVaccinated());
+        assertEquals(catRequest.veterinarianName(), cat.getVeterinarianName());
+        assertEquals(catRequest.phoneVet(), cat.getPhoneVet());
+        assertEquals(catRequest.medicationName(), cat.getMedicationName());
+        assertEquals(catRequest.medicationDose(), cat.getMedicationDose());
+        assertEquals(catRequest.ownerUsername(), cat.getOwner()); // hier gaat iets mis
     }
 }
+
+
