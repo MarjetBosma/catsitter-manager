b1d051c0f2950150fe63b9fe6a773f8ffd75b8c8
Bosma <marjet.bosma@sogeti.com>
2024-05-17 10:21:01 +0200

    wijzigingen tests, nog steeds werk in uitvoering

--- src/main/java/nl/novi/catsittermanager/CatsittermanagerApplication.java
+++ src/main/java/nl/novi/catsittermanager/CatsittermanagerApplication.java
@@ -1,11 +1,8 @@
 package nl.novi.catsittermanager;
 
-import nl.novi.catsittermanager.config.JacksonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-
 @ComponentScan(basePackages = {"nl.novi.catsittermanager"})
 @SpringBootApplication
 public class CatsittermanagerApplication {
--- src/main/java/nl/novi/catsittermanager/exceptions/RecordNotFoundException.java
+++ src/main/java/nl/novi/catsittermanager/exceptions/RecordNotFoundException.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 
 public class RecordNotFoundException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
     public RecordNotFoundException(HttpStatus notFound, String s) {
         super(s);
     }
